5a16bf0c0f80b5181341728e73df51589eb4f878
karankumarshreds <naileditnerd@gmail.com>
2021-09-27 23:18:48 +0530

    add main

--- user_service/cmd/main.go
+++ user_service/cmd/main.go
@@ -1,6 +1,20 @@
 package main 
 
 import (
-	"github.com/karankumarshreds/Clone/app"
+	"github.com/karankumarshreds/Clone/user_service/cmd/app"
 ) 
 
+func main() {
+
+	a := app.App{}
+	a.Initialize(
+		"8000",
+		"development",
+		app.DB_CONFIG{
+			User: "user_service",
+			Database: "users_db",
+			Password: "password",
+		},
+	)
+	a.Run()
+}
